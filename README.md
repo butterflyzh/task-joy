ac7416362cd0a07530c2cc66f62a296830f18ccd
butterflyzh <butterflyzhu@126.com>
2022-02-16 11:31:10 +0800

    增加例子

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 # task-joy
-task-joy是仿写Spring的Task模块，使用注解@EnableScheduling2和@Scheduled2来定时任务。
+task-joy是仿写Spring的Task模块，简化Spring一些复杂步骤，使用注解@EnableScheduling2和@Scheduled2来定时任务。
 
+
+##### 效果
+编写启动类型，使用`@EnableScheduling2` 注解
+```java
+@SpringBootApplication
+@EnableScheduling2
+public class TaskJoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskJoyApplication.class, args);
+    }
+
+}
+```
+
+使用`@Scheduled2`注解创建一个任务
+```java
+@Component
+public class MyTask {
+
+    @Scheduled2(value = "task1", cron = "*/5 * * * * ?")
+    public void task() {
+        System.out.println("执行了");
+    }
+}
+```
+
+##### 运行
+![](./img/result.png)
  
--- src/main/java/com/butterflyzhu/taskjoy/task/MyTask.java
+++ src/main/java/com/butterflyzhu/taskjoy/task/MyTask.java
@@ -10,38 +10,17 @@ import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProc
 import org.springframework.scheduling.config.ScheduledTask;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.ScheduledFuture;
 
 @Component
 public class MyTask {
 
-    @Autowired
-    private ScheduledAnnotationBeanPostProcessor2 scheduledAnnotationBeanPostProcessor;
-
     @Scheduled2(value = "task1", cron = "*/5 * * * * ?")
     public void task() {
-        System.out.println("执行了");
-    }
 
-    @Scheduled2(value = "task2", cron = "*/10 * * * * ?")
-    public void task2() {
-        System.out.println("执行了2");
-        Set<ScheduledTask2> tasks = scheduledAnnotationBeanPostProcessor.getScheduledTasks();
-        System.out.println(tasks.size());
-        for (ScheduledTask2 scheduledTask2 : tasks) {
-            if (scheduledTask2.getTask() instanceof CronTaskAdapter && scheduledTask2.getScheduledFuture() != null) {
-                CronTaskAdapter cronTaskAdapter = (CronTaskAdapter) scheduledTask2.getTask();
-                System.out.println("任务: " + cronTaskAdapter.getTaskName());
-                ScheduledFuture<?> future = scheduledTask2.getScheduledFuture();
-                if (future.isCancelled()) {
-                    System.out.println("取消");
-                }
-                if (future.isDone()) {
-                    System.out.println("完成");
-                }
-                System.out.println(future.isCancelled() + " " + future.isDone());
-            }
-        }
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + ": 执行了");
     }
 }
